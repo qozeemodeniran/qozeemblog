a74e1c9d041fe730613f7c09dfd875e63818ebef
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-10-30 08:25:39 +0100

    added database variables

--- config.php
+++ config.php
@@ -3,7 +3,9 @@
 	session_start();
 
 	// connect to database
-    $conn = mysqli_connect("localhost", "root", "", "b12_27082892_qozeemblog");
+	$conn = mysqli_connect("us-cdbr-east-02.cleardb.com", "b59c7b7f1ea18c", "49b034f4", "heroku_d50ce22599704b0");
+	
+	mysql://b59c7b7f1ea18c:49b034f4@us-cdbr-east-02.cleardb.com/heroku_d50ce22599704b0?reconnect=true
 
     if (!$conn) {
     	die("Failed database connection" . mysqli_connect_error());
