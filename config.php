136e39fe694a725a041fb08c4fd7c968c84997cc
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-11-19 07:03:33 +0100

    configured db

--- config.php
+++ config.php
@@ -3,9 +3,8 @@
 	session_start();
 
 	// connect to database
-	$conn = mysqli_connect("us-cdbr-east-02.cleardb.com", "b59c7b7f1ea18c", "49b034f4", "heroku_d50ce22599704b0");
+	$conn = mysqli_connect("us-cdbr-east-02.cleardb.com", "b80503685742be", "f29aaf6a", "heroku_f56cc8dd3618c46");
 	
-	mysql://b59c7b7f1ea18c:49b034f4@us-cdbr-east-02.cleardb.com/heroku_d50ce22599704b0?reconnect=true
 
     if (!$conn) {
     	die("Failed database connection" . mysqli_connect_error());
