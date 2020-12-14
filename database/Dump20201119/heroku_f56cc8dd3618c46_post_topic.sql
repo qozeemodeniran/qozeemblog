6ed00ba0f07b278e970e269901cf7da07afb7bfd
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-12-14 21:58:12 +0100

    added custome domain

---
+++ database/Dump20201119/heroku_f56cc8dd3618c46_post_topic.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: us-cdbr-east-02.cleardb.com    Database: heroku_f56cc8dd3618c46
+-- ------------------------------------------------------
+-- Server version	5.5.62-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `post_topic`
+--
+
+DROP TABLE IF EXISTS `post_topic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_topic` (
+  `id` int(11) DEFAULT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `topic_id` int(11) DEFAULT NULL,
+  UNIQUE KEY `post_id` (`post_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post_topic`
+--
+
+LOCK TABLES `post_topic` WRITE;
+/*!40000 ALTER TABLE `post_topic` DISABLE KEYS */;
+INSERT INTO `post_topic` VALUES (1,1,1),(2,2,2),(NULL,11,2),(NULL,21,1),(NULL,31,1),(NULL,41,4),(NULL,51,1),(NULL,61,3),(NULL,71,4),(NULL,0,4),(NULL,81,4),(NULL,91,4),(NULL,101,3),(NULL,111,6),(NULL,121,1),(NULL,131,4),(NULL,141,4),(NULL,151,4),(NULL,161,1),(NULL,171,5);
+/*!40000 ALTER TABLE `post_topic` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-19  7:32:26
