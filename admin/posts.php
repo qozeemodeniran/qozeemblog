aeebb6c4285669a807626acb4b69fc788de29a58
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-02-10 16:27:05 +0100

    updates

--- admin/posts.php
+++ admin/posts.php
@@ -49,7 +49,7 @@
 							</td>
 							<td><?php echo $post['author_name']; ?></td>
 							<td>
-							<?php $query = mysql_query(" UPDATE posts SET views + 1 WHERE id = '$id' "); ?></td>
+							<?php echo $post['views']; ?></td>
 							
 							<!-- Only Admin can publish/unpublish post -->
 							<?php if ($_SESSION['user']['role'] == "Admin" ): ?>
