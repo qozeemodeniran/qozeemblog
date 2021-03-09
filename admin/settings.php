d5458d4756493ac7b7b8b30f4e3d8311c4084bc3
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-03-09 14:59:16 +0100

    updates

--- admin/settings.php
+++ admin/settings.php
@@ -28,43 +28,7 @@
 
 
             Choose a logo:
-            <input type="file" 
-                name="uploadfile" 
-                value="" 
-            /> 
-            <div> 
-                <button type="submit"
-                        name="upload"> 
-                  UPLOAD 
-                </button> 
-            </div> 
-            <?php error_reporting(0); ?>
-            <?php 
-            $msg="";
-
-                if (isset($_POST['upload'])) { 
-  
-                    $filename = $_FILES["uploadfile"]["name"]; 
-                    $tempname = $_FILES["uploadfile"]["tmp_name"];     
-                        $folder = "image/".$filename; 
-                          
-                    $db = mysqli_connect("us-cdbr-east-03.cleardb.com", "b5a8056a6aa74f", "b5a8056a6aa74f", "heroku_e6dc99b8c0a5bb2"); 
-                  
-                        // Get all the submitted data from the form 
-                        $sql = "INSERT INTO image (filename) VALUES ('$filename')"; 
-                  
-                        // Execute query 
-                        mysqli_query($db, $sql); 
-                          
-                        // Now let's move the uploaded image into the folder: image 
-                        if (move_uploaded_file($tempname, $folder))  { 
-                            $msg = "Image uploaded successfully"; 
-                        }else{ 
-                            $msg = "Failed to upload image"; 
-                      } 
-                } 
-                  $result = mysqli_query($db, "SELECT * FROM image"); 
-            ?>
+            <input type="file" name="file">
             <input type="text" name="email" value="" placeholder="Email">
             <input type="text" name="phone" value="" placeholder="Phone nUmber">
 
