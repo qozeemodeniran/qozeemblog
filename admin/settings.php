75050ab03abd36a654707390b9ff958649e244cf
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-03-09 12:25:52 +0100

    updates on setting

--- admin/settings.php
+++ admin/settings.php
@@ -8,3 +8,17 @@
 <?php include(ROOT_PATH . '/admin/includes/head_section.php'); ?>
 	<title>Admin | Global Settings</title>
 </head>
+<body>
+    <!-- admin navbar -->
+	<?php include(ROOT_PATH . '/admin/includes/navbar.php') ?>
+    <div class="menu">
+	<div class="card">
+		<div class="card-header">
+			<h2>Actions</h2>
+		</div>
+		<div class="card-content">
+        <input type="file" name="featured_image" > Upload logo
+		</div>
+	</div>
+</div>
+</body>
