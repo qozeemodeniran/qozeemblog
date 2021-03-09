948f6e1be8f5ea8458cbb9952b1080102a8778a8
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-03-09 12:58:22 +0100

    update

--- admin/settings.php
+++ admin/settings.php
@@ -17,8 +17,12 @@
 			<h2>Actions</h2>
 		</div>
 		<div class="card-content">
-        <input type="file" name="featured_image" > Upload logo
+			<a href="<?php echo BASE_URL . 'admin/create_post.php' ?>"></a>
+			<a href="<?php echo BASE_URL . 'admin/posts.php' ?>">Manage Posts</a>
+			<a href="<?php echo BASE_URL . 'admin/users.php' ?>">Manage Users</a>
+			<a href="<?php echo BASE_URL . 'admin/topics.php' ?>">Manage Topics</a>
 		</div>
 	</div>
+</div>
 </div>
 </body>
