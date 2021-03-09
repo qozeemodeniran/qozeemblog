ca5837995a8f0900c93b77f7ed3bd9287b5a27bb
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-03-09 13:11:04 +0100

    updates

--- admin/settings.php
+++ admin/settings.php
@@ -1,3 +1,5 @@
+
+
 <?php  include('../config.php'); ?>
 <?php  include(ROOT_PATH . '/admin/includes/admin_functions.php'); ?>
 <?php 
@@ -11,18 +13,4 @@
 <body>
     <!-- admin navbar -->
 	<?php include(ROOT_PATH . '/admin/includes/navbar.php') ?>
-    <div class="menu">
-	<div class="card">
-		<div class="card-header">
-			<h2>Actions</h2>
-		</div>
-		<div class="card-content">
-			<a href="<?php echo BASE_URL . 'admin/create_post.php' ?>"></a>
-			<a href="<?php echo BASE_URL . 'admin/posts.php' ?>">Manage Posts</a>
-			<a href="<?php echo BASE_URL . 'admin/users.php' ?>">Manage Users</a>
-			<a href="<?php echo BASE_URL . 'admin/topics.php' ?>">Manage Topics</a>
-		</div>
-	</div>
-</div>
-</div>
 </body>
