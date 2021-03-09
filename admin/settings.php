adecf18e3b6be3441d01a7dda40ea18dc915f7f1
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-03-09 13:37:25 +0100

    updates

--- admin/settings.php
+++ admin/settings.php
@@ -15,23 +15,4 @@
 	<?php include(ROOT_PATH . '/admin/includes/navbar.php') ?>
 
     <h1 style="text-align: center; margin-top: 20px;">Update Blog Info</h1>
-        <form action="" method="post">
-            <label for="logo">
-            Upload Logo: 
-                <input type="file">
-            </label>
-            <br>
-            <label for="email">
-                Email: 
-                <input type="text">
-            </label>
-            <br>
-            <label for="phone">
-            Phone Number: 
-                <input type="text">
-            </label>
-            <br>
-
-            <input type="submit" value="Update">
-        </form>
 </body>
