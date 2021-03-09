347d3ad2065aa3357fa1c7291ec7cfc41889ca41
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-03-09 15:57:55 +0100

    updates

--- admin/settings.php
+++ admin/settings.php
@@ -16,23 +16,14 @@
 
     <div>
         <h1 style="text-align: center; margin-top: 20px;">Update Blog Info</h1>
-        <form method="post" enctype="multipart/form-data" action="" >
-
-                <!-- validation errors for the form -->
-				<?php include(ROOT_PATH . '/includes/errors.php') ?>
-
-				<!-- if editing user, the id is required to identify that user -->
-				<?php if ($isEditingUser === true): ?>
-					<input type="hidden" name="admin_id" value="<?php echo $admin_id; ?>">
-				<?php endif ?>
-
+        <form method="post" enctype="multipart/form-data" action="upload.php" >
 
             Choose a logo:
-            <input type="file" name="file">
-            <input type="text" name="email" value="" placeholder="Email">
-            <input type="text" name="phone" value="" placeholder="Phone nUmber">
-
-            <button type="submit" class="btn" name="update_admin">UPDATE</button>
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+            <!-- <input type="text" name="email" value="" placeholder="Email">
+            <input type="text" name="phone" value="" placeholder="Phone nUmber"> -->
+            <!-- <button type="submit" class="btn" name="update_admin">UPDATE</button> -->
         </form>
     </div>
 </body>
--- config.php
+++ config.php
@@ -4,8 +4,6 @@
 	// connect to database
 	$conn = mysqli_connect("us-cdbr-east-03.cleardb.com", "b5a8056a6aa74f", "c21078ea", "heroku_e6dc99b8c0a5bb2");
 	
-	// mysql://b59c7b7f1ea18c:49b034f4@us-cdbr-east-02.cleardb.com/heroku_d50ce22599704b0?reconnect=true
-
     if (!$conn) {
     	die("Failed database connection" . mysqli_connect_error());
     }
