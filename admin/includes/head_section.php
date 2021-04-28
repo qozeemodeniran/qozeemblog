85e87b11b318109db8e626cd394ed4a258793138
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-04-28 15:38:25 +0100

    minor fixes

--- admin/includes/head_section.php
+++ admin/includes/head_section.php
@@ -3,6 +3,7 @@
 <html lang="en-US">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 <meta name="description" content="<?php echo $pageDescription?>">
 <meta name="seo" content="<?php echo $keywords ?>">  
 <!-- Google Fonts -->
