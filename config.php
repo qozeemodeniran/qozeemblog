58318085eab5f75c4d6eb82e165c47ac4cc98b1f
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-04-28 12:31:25 +0100

    deploying to heroku

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php 
 	session_start();
 
-	// connect to database
-	$conn = mysqli_connect("localhost", "root", "", "qozeemblog"); // localhost connection
+	// // connect to database
+	// $conn = mysqli_connect("localhost", "root", "", "qozeemblog"); // localhost connection
 
-	// $conn = mysqli_connect("us-cdbr-east-03.cleardb.com", "b3517ce3da674a", "608391b2", "heroku_c54e62c766ebe15"); //mysql workspace connection (for heroku)
+	$conn = mysqli_connect("us-cdbr-east-03.cleardb.com", "b3517ce3da674a", "608391b2", "heroku_c54e62c766ebe15"); //mysql workspace connection (for heroku)
 
 	// $conn = mysqli_connect("sql301.byethost32.com", "b32_27687645", "lastborn231", "b32_27687645_qozeemblog"); // byethost
 
@@ -16,9 +16,9 @@
 
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
 
-	define('BASE_URL', 'http://qozeemblog.localhost/'); // localhost host url 
+	// define('BASE_URL', 'http://qozeemblog.localhost/'); // localhost host url 
 
-	// define('BASE_URL', 'https://qozeemblog.herokuapp.com/'); // heroku url
+	define('BASE_URL', 'https://qozeemblog.herokuapp.com/'); // heroku url
 
 	// define('BASE_URL', 'http://qozeemodeniran.byethost32.com/qozeemblog/index.php') // byethost url
 ?>
