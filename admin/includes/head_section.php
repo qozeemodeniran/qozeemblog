50fd76a484a0457cd2d7a4c9595d85a6ec007c2e
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-04-30 11:33:40 +0100

    fixing

--- admin/includes/head_section.php
+++ admin/includes/head_section.php
@@ -1,9 +1,10 @@
 
 <!DOCTYPE html>
-<html lang="en-US">
+<html lang="en">
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="<?php echo $pageDescription?>">
 <meta name="seo" content="<?php echo $keywords ?>">  
 <!-- Google Fonts -->
