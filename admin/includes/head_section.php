2932681e4bdbac530d67eb8ea39addd862a95ed0
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-04-30 11:42:45 +0100

    fixing

--- admin/includes/head_section.php
+++ admin/includes/head_section.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="<?php echo $pageDescription?>">
 <meta name="seo" content="<?php echo $keywords ?>">  
