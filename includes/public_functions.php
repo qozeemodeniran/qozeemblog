1429e44b3769f445f56a7513600d49cf836ed7fb
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-05-10 13:41:08 +0100

    partially done with comment system

--- includes/public_functions.php
+++ includes/public_functions.php
@@ -94,47 +94,50 @@ function getAllTopics()
 	return $topics;
 }
 
-// /**
-//  * Comments functions
-//  */
-// // Set logged in user id: This is just a simulation of user login. We haven't implemented user log in
-// 	// But we will assume that when a user logs in, 
-// 	// they are assigned an id in the session variable to identify them across pages
-// 	$user_id = 1;
-// 	// connect to database
-// 	global $conn;
-// 	// get post with id 191 from database
-// 	$post_query_result = mysqli_query($conn, "SELECT * FROM posts WHERE id=191");
-// 	$post = mysqli_fetch_assoc($post_query_result);
+/**
+ * Comment function
+ */
+// Setting logged-in user id...
+$user_id = 103;
 
-// 	// Get all comments from database
-// 	// $comments_query_result = mysqli_query($conn, "SELECT * FROM comments WHERE post_id=" . $post['id'] . " ORDER BY created_at DESC");
-// 	// $comments = mysqli_fetch_all($comments_query_result, MYSQLI_ASSOC);
+// Database connection
+global $conn;
 
-// 	// Receives a user id and returns the username
-// 	function getUsernameById($id)
-// 	{
-// 		global $conn;
-// 		$result = mysqli_query($conn, "SELECT username FROM users WHERE id=" . $id . " LIMIT 1");
-// 		// return the username
-// 		return mysqli_fetch_assoc($result)['username'];
-// 	}
-// 	// Receives a comment id and returns the username
-// 	function getRepliesByCommentId($id)
-// 	{
-// 		global $conn;
-// 		$result = mysqli_query($conn, "SELECT * FROM replies WHERE comment_id=$id");
-// 		$replies = mysqli_fetch_all($result, MYSQLI_ASSOC);
-// 		return $replies;
-// 	}
-// 	// Receives a post id and returns the total number of comments on that post
-// 	function getCommentsCountByPostId($post_id)
-// 	{
-// 		global $conn;
-// 		$result = mysqli_query($conn, "SELECT COUNT(*) AS total FROM comments");
-// 		$data = mysqli_fetch_assoc($result);
-// 		return $data['total'];
-// 	}
+// Get post with the id 195 from the database
+$post_query_result = mysqli_query($conn, "SELECT * FROM comments WHERE post_id=195");
+$post = mysqli_fetch_assoc($post_query_result);
+
+// Getting all comments form databse
+$comments_query_result = mysqli_query($conn, "SELECT * FROM comments WHERE post_id=" . $post['id'] . " ORDER BY created_at DESC");
+
+$comments = mysqli_fetch_all($comments_query_result, MYSQLI_ASSOC); 
+
+// Return username on receiving user id
+function getUsernameById($id){
+	global $conn;
+	$result = mysqli_query($conn, "SELECT username FROM users WHERE id=" . $id . "limit 1");
+
+	// Return username
+	return mysqli_fect_assoc($result)['username'];
+}
+
+// Return username on receiving comment id
+function getRepliesByCommentId($id){
+	global $conn;
+	$result = mysqli_query($conn, "SELECT * FROM replies WHERE comment_id=$id");
+	$replies = mysqli_fecth_all($result, MYSQLI_ASSOC);
+	return $replies;
+}
+
+// Return total number of comment on a post on receiving the post's id
+function getCommentsCountByPostId($post_id){
+	global $conn;
+	$result = mysqli_query($conn, "select count(*) as total from comments");
+	$data = mysqli_fecth_assoc($result);
+	return $data['total'];
+}
+
+
 
 // // If the user clicked submit on comment form...
 // if (isset($_POST['comment_posted'])) {
--- single_post.php
+++ single_post.php
@@ -59,6 +59,10 @@
 		
 		<!-- Comment section -->
 		<div class="col-md-6 col-md-offset-0 comments-section">
+
+		<!-- If user user is not signed in, requeste them to sign in. If user is signed in, present them comment form -->
+		<?php if(isset($user_id)): ?>
+
 		    <!-- comment form -->
 			<form class="clearfix" action="" method="post" id="comment_form">
 				<h4>Post a comment:</h4>
@@ -66,34 +70,64 @@
 				<button class="btn btn-primary btn-sm pull-right" id="submit_comment">Submit comment</button>
 			</form>
 			<!-- // comment form -->
+		<?php else: ?>
+			<div class="well" style="margin-top: 20px;">
+				<h4 class="text-center"><a href="#">Sign in</a> to post a comment</h4>
+			</div>
+		<?php endif; ?>
 
 			<!-- Display total number of comments on this post  -->
-			<h2><span id="comments_count">0</span> Comment(s)</h2>
+			<h2><span id="comments_count"><?php echo count($comments) ?></span> Comment(s)</h2>
 			<hr>
 
 			<!-- comments wrapper -->
 			<div id="comments-wrapper">
+				<?php if(isset($comments)): ?>
+
+				<!-- Displaying comments -->
+				<?php foreach($comments as $comment): ?>
+
+				<!-- Comment -->
 				<div class="comment clearfix">
 						<img src="profile.png" alt="" class="profile_pic">
 						<div class="comment-details">
-							<span class="comment-name">Qozeem</span>
-							<span class="comment-date">Apr 24, 2018</span>
-							<p>This is the first reply to this post on this website.</p>
-							<a class="reply-btn" href="#" >reply</a>
+							<span class="comment-name"><?php echo getUsernameById($comment['user_id']); ?></span>
+							<span class="comment-date"><?php echo data("F, j, Y", strtotime($comment['created_at'])); ?></span>
+							<p><?php echo $comment['body']; ?></p>
+							<a class="reply-btn" href="#" data-id="<?php echo $comment['id']; ?>" >reply</a>
 						</div>
-						<div>
-							<!-- reply -->
-							<div class="comment reply clearfix">
-								<img src="profile.png" alt="" class="profile_pic">
-								<div class="comment-details">
-									<span class="comment-name">Pixie Technologies</span>
-									<span class="comment-date">Apr 24, 2018</span>
-									<p>Hey, why are you the first to comment on this post?</p>
-									<a class="reply-btn" href="#">reply</a>
-								</div>
-							</div>
+
+						<!-- reply form -->
+						<form action="post_details.php" class="reply_form clearfix" id="comment_reply_form_<?php echo $comment['id'] ?>" data-id="<?php echo $comment['id']; ?>">
+							<textarea class="form-control" name="reply_text" id="reply_text" cols="30" rows="2"></textarea>
+							<button class="btn btn-primary btn-xs pull-right submit-reply">Submit reply</button>
+						</form>
+
+						<!-- Getting all replies -->
+						<?php $replies=getRepliesByCommentId($comment['id']); ?>
+						<div class="replies_wrapper_<?php echo $comment['id']; ?>">
+							<?php if (isset($replies)): ?>
+								<?php foreach ($replies as $reply): ?>
+									<!-- reply -->
+									<div class="comment reply clearfix">
+										<img src="profile.png" alt="" class="profile_pic">
+										<div class="comment-details">
+											<span class="comment-name"><?php echo getUsernameById($reply['user_id']) ?></span>
+											<span class="comment-date"><?php echo date("F j, Y ", strtotime($reply["created_at"])); ?></span>
+											<p><?php echo $reply['body']; ?></p>
+											<a class="reply-btn" href="#">reply</a>
+										</div>
+									</div>
+								<?php endforeach; ?>
+							<?php endif; ?>
 						</div>
 					</div>
+				</div>
+				<!-- //comment -->
+				<?php endforeach; ?>
+				<?php else: ?>
+					<h2>Be the first to comment on this post.</h2>
+				<?php endif; ?>
 			</div>
 			<!-- // comments wrapper -->
 		</div>
